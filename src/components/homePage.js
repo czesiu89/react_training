f4adbad2d5090c2cece448cde3259e9bd09adf0d
Czech <pawel.czech@nokia.com>
2016-09-02 07:22:53 +0200

    first one

---
+++ src/components/homePage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Admin</h1>
+                <p>React, React Router and Flux for ultra responsive Web App</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
+
