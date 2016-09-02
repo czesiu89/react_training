fbf39b722eafeed43957d4547b26fe8efc25f30d
Czech <pawel.czech@nokia.com>
2016-09-02 14:57:40 +0200

    Backup

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,20 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read this shitty page?')) {
+                transition.about();
+            } else {
+                callback();
+            }            
+        },
+        willTransitionFrom: function(transition, component) {
+            if (!confirm('Are you sure you want to leave the page?')) {
+                transition.about();
+            }       
+        }   
+    },
     render: function () {
         return (
             <div>
