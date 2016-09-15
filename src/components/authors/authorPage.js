69cc4d45cefea8ef5c4248c14548a7eada873de4
Czech <pawel.czech@nokia.com>
2016-09-15 15:09:26 +0200

    BEfore flux

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -7,25 +7,27 @@ var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
 var AuthorPage = React.createClass({
-    getInitialState: function () {
-        return {
-            authors: []
-        };
-    },
-    componentDidMount: function () {
-		if(this.isMounted()){
-            this.setState({ authors: AuthorApi.getAllAuthors() });
-        }
-    },
-    render: function () {
-        return (
-            <div>
-                <h1>Authors</h1>
-                <Link to="addAuthor" className="btn btn-default">Add Author</Link>
-                <AuthorList authors={this.state.authors} />
-            </div>
-        );
-    }
+	getInitialState: function() {
+		return {
+			authors: []
+		};
+	},
+
+	componentDidMount: function() {
+		if (this.isMounted()) {
+			this.setState({ authors: AuthorApi.getAllAuthors() });
+		}
+	},
+
+	render: function() {
+		return (
+			<div>
+				<h1>Authors</h1>
+				<Link to="addAuthor" className="btn btn-default">Add Author</Link>
+				<AuthorList authors={this.state.authors} />
+			</div>
+		);
+	}
 });
 
 module.exports = AuthorPage;
